c61e30b3f48991b012710855fa9d7e5cfb74d59d
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-20 11:28:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let navigator={
+	browserName:"Netscape",
+	version:"5.0(Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 OPR/94.0.0.0"
+}
+let divElement=document.getElementById("browser-info");
+divElement.innerText=`You are using ${navigator.browserName} version ${navigator.version}`;
